8bcb50647250a0b8d8e5872596eb3250f04f8df1
Olha Bondarenko <olha_bondarenko64@ukr.net>
2020-08-18 00:44:20 +0300

    Updates

---
+++ precache-manifest.b7b61bf52d3e2159fd74640d751b9fa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b01c562f8cd33d45d384d4e41015ddd4",
+    "url": "/react-calc-money/index.html"
+  },
+  {
+    "revision": "cd2b128ef65c01feb558",
+    "url": "/react-calc-money/static/css/main.9e034465.chunk.css"
+  },
+  {
+    "revision": "4bfcfcfc96f2e78dfe0d",
+    "url": "/react-calc-money/static/js/2.99bf90c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calc-money/static/js/2.99bf90c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd2b128ef65c01feb558",
+    "url": "/react-calc-money/static/js/main.6819a738.chunk.js"
+  },
+  {
+    "revision": "d31d5a9bdb921ed041e1",
+    "url": "/react-calc-money/static/js/runtime-main.5583a0c2.js"
+  }
+]);
